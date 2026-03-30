560cdbfce70cdbbdf26258273821c7dbe91c2af2
vivekAdventz <vivek.kumar@adventz.com>
2026-03-30 17:39:05 +0530

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/timesheet';
+const MONGODB_URI = process.env.MONGODB_URI ;
 
 export async function connectDB() {
   await mongoose.connect(MONGODB_URI);
