324a33ef49d7c376288d7c71227f5db4319505be
vivekAdventz <vivek.kumar@adventz.com>
2026-03-30 17:43:46 +0530

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI ;
-
 export async function connectDB() {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) {
+    throw new Error('MONGODB_URI is not defined in environment variables');
+  }
   await mongoose.connect(MONGODB_URI);
   console.log('Connected to MongoDB');
 }
